6d45928bac0604bee28615a3289e6f63938f34ce
Mathias Johansson <32643926+Mathiasgreyit@users.noreply.github.com>
2024-05-22 12:53:27 +0200

    Exchange online scripts (#10)
    * Exchange scripts

---
+++ ExchangeOnline/Get-majoOutBoundConnectorTLS.ps1
@@ -0,0 +1,70 @@
+<#
+.SYNOPSIS
+    Retrieves outbound connectors with TLS settings and their recipient domains.
+
+.DESCRIPTION
+    This script retrieves outbound connectors with TLS settings and their recipient domains. 
+    It provides the option to filter by duplicate domains and specific connector identities or domains.
+
+.PARAMETER OnlyDuplicateDomains
+    Specifies whether to only return duplicate recipient domains.
+    By default, all recipient domains are returned.
+
+.PARAMETER Identity
+    Specifies the identity of the outbound connector to retrieve.
+    If not specified, all outbound connectors are retrieved.
+
+.PARAMETER Domain
+    Specifies the recipient domains to filter by.
+    Only outbound connectors with matching recipient domains are returned.
+
+.EXAMPLE
+    Get-majoOutBoundConnectorTLS.ps1 -OnlyDuplicateDomains
+    Retrieves outbound connectors with duplicate recipient domains.
+
+.EXAMPLE
+    Get-majoOutBoundConnectorTLS.ps1 -Identity "Connector1"
+    Retrieves the outbound connector with the specified identity.
+
+.EXAMPLE
+    Get-majoOutBoundConnectorTLS.ps1 -Domain "example.com", "contoso.com"
+    Retrieves outbound connectors with the specified recipient domains.
+
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [switch]$OnlyDuplicateDomains,
+    [Parameter(Mandatory = $false)]
+    [string]$Identity,
+    [Parameter(Mandatory = $false)]
+    [string[]]$Domain
+)
+begin {
+    # Check if we have name Outboundconnector with force TLS on
+    $tlsconnectors = Get-OutboundConnector | Where-Object { $_.TlsSettings -eq 'CertificateValidation' }
+    
+}
+process {
+    $recdom = @()   
+    foreach ($connector in $tlsconnectors) {
+        foreach ($dom in $connector.RecipientDomains) {
+            if ($Domain -and $Domain -notcontains $dom) { continue }
+            $myobj = New-Object -TypeName PSObject -Property @{
+                'ConnectorId'     = $connector.Identity
+                'RecipientDomain' = $dom
+                'Enabled'         = $connector.Enabled
+            }
+            $recdom += $myobj
+        }
+    }
+}
+end {
+    if ($OnlyDuplicateDomains) {
+        $recdom | Group-Object -Property RecipientDomain | Where-Object { $_.Count -gt 1 } | Select-Object -ExpandProperty Group
+    }
+    else {
+        $recdom
+    }
+}
---
+++ ExchangeOnline/Readme.md
@@ -0,0 +1,37 @@
+# PowerShell Scripts
+
+This repository contains PowerShell scripts for managing outbound connectors in Exchange Online.
+
+## Scripts
+
+### Add-majoOutboundConnectorTLS.ps1
+
+This script adds or updates an outbound connector in Exchange Online with TLS settings for specified external domains. If an outbound connector with the specified name already exists, the script updates the recipient domains of the connector. If the outbound connector does not exist, a new outbound connector is created.
+
+#### Parameters
+
+- `Domains`: Specifies an array of external domains for which TLS settings should be applied.
+- `Identity`: Specifies the name of the outbound connector. Default value is 'External TLS 1'.
+- `Comment`: Specifies a comment for the outbound connector. Default value is 'Outbound connector with force TLS settings for external domains.'
+
+#### Example
+
+```powershell
+Add-majoOutboundConnectorTLS.ps1 -Domains "example.com", "contoso.com" -Identity "External TLS 2" -Comment "Outbound connector for TLS 2"
+```
+### Get-majoOutBoundConnectorTLS.ps1
+
+This script retrieves outbound connectors with TLS settings and their recipient domains. It provides the option to filter by duplicate domains and specific connector identities or domains.
+
+#### Parameters
+
+- `OnlyDuplicateDomains`: Specifies whether to only return duplicate recipient domains. By default, all recipient domains are returned.
+- `Identity`: Specifies the identity of the outbound connector to retrieve. If not specified, all outbound connectors are retrieved.
+- `Domain`: Specifies the recipient domains to filter by. Only outbound connectors with matching recipient domains are returned.
+
+#### Example
+
+```powershell
+Get-majoOutBoundConnectorTLS.ps1 -OnlyDuplicateDomains
+Get-majoOutBoundConnectorTLS.ps1 -OnlyDuplicateDomains -Domain "example.com"  
+Get-majoOutBoundConnectorTLS.ps1 -OnlyDuplicateDomains -Identity "Connector1" -Domain "example.com"
